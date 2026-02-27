fed4f676b06acb8ec89afa58ae38c3464707973c
nojunhui <shwnsgml0523@naver.com>
2026-02-27 17:32:10 +0900

    second commit

---
+++ src/ch01_variable_operator/Q2.java
@@ -0,0 +1,19 @@
+package ch01_variable_operator;
+
+public class Q2 {
+    static void main() {
+        int x = 3, y = 5, z;
+
+        z = x++ + --y;
+
+        System.out.println("x : " + x); // 4
+        System.out.println("y : " + y); // 4
+        System.out.println("z : " + z); // 7
+
+        z += --x + y++;
+
+        System.out.println("x : " + x); // 3
+        System.out.println("y : " + y); // 5
+        System.out.println("z : " + z); // 14
+    }
+}
