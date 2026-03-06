08b60acbfb66b6630ec6e52bdaf0a142a1d9b63e
nojunhui <shwnsgml0523@naver.com>
2026-03-06 13:15:34 +0900

    03/ 05

--- src/ch02_control_statement/study.java
+++ src/ch02_control_statement/study.java
@@ -6,21 +6,26 @@ public class study {
         int i = 0;
         String star = "*";
 
-        for (i = 0; i < 4; i++) {
+        for (int t = 0; t < 2; t++) {
+            for (i = 0; i < 4; i++) {
 
-            int s = 1 + 2*i; // 별 개수
+                int s = 1 + 2 * i; // 별 개수
 
-            // 공백 출력
-            for (int k = 0; k < 3 - i; k++) {
-                System.out.print(" ");
+                // 공백 출력
+                for (int k = 0; k < 3 - i; k++) {
+                    System.out.print(" ");
+                }
+
+                // 별 출력
+                for (int j = 0; j < s; j++) {
+                    System.out.print(star);
+                }
+
+                System.out.println(); // 줄바꿈
             }
-
-            // 별 출력
-            for (int j = 0; j < s; j++) {
-                System.out.print(star);
-            }
-
-            System.out.println(); // 줄바꿈
         }
+            System.out.println("  ㅣㅣ  ");
+            System.out.println("   ㅡ  ");
+
     }
 }
